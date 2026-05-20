0a7c7d0199738d4d896e2dae92526190c88f8de2
Lautaro Gartner <158626277+LautaroGartner@users.noreply.github.com>
2026-05-20 09:30:11 -0300

    Add Paideia positioning essay

--- src/writing/index.ts
+++ src/writing/index.ts
@@ -1,7 +1,9 @@
 import { post as firstPost } from "./first-post.js";
+import { post as buildingPaideiaPost } from "./building-paideia-software-that-explains-itself.js";
 import { post as generatedSystemsPost } from "./generated-systems-should-explain-themselves.js";
 
 export const posts = [
+  buildingPaideiaPost,
   generatedSystemsPost,
   firstPost,
 ];
